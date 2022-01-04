0bb3224910764f44116dbca60b0205167076d439
tjrgusdl41 <63176593+tjrgusdl41@users.noreply.github.com>
2022-01-04 16:55:10 +0900

    content 스타일링중..

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,14 +1,23 @@
 import React from "react";
+import logo from "../img/logo.png";
 const Content = () => {
   return (
     <div className="PageTemplate-contents">
       <div className="HomeTemplate">
         <div className="HomeTemplate-header">대회 목록</div>
         <div className="HomeTemplate-content">
-          <div>
+          <div className="content-card">
+            <img src={logo}></img>
             <p>user</p>
             <h2>코딩테스트</h2>
             <p>마감n일전</p>
+            <button>대회 신청하기</button>
+          </div>
+          <div className="content-card-right">
+            <p>user</p>
+            <h2>코딩테스트</h2>
+            <p>마감n일전</p>
+            <button>대회 신청하기</button>
           </div>
         </div>
       </div>
