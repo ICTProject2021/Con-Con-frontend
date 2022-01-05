1be91862b7c06bf91938830ab19cc93550ce118e
tjrgusdl41 <63176593+tjrgusdl41@users.noreply.github.com>
2022-01-05 12:52:01 +0900

    Home content 구조잡기 스타일링

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,18 +6,45 @@ const Content = () => {
       <div className="HomeTemplate">
         <div className="HomeTemplate-header">대회 목록</div>
         <div className="HomeTemplate-content">
-          <div className="content-card">
-            <img src={logo}></img>
-            <p>user</p>
-            <h2>코딩테스트</h2>
-            <p>마감n일전</p>
-            <button>대회 신청하기</button>
+          <div className="HomeTemplate-content-left">
+            <div className="HomeTemplate-content-left-top">
+              <div className="content-card1">
+                <img src={logo}></img>
+                <p>user</p>
+                <h2>코딩테스트</h2>
+                <p>마감n일전</p>
+                <button>대회 신청하기</button>
+              </div>
+              <div className="content-card2">
+                <img src={logo}></img>
+                <p>user</p>
+                <h2>코딩테스트</h2>
+                <p>마감n일전</p>
+                <button>대회 신청하기</button>
+              </div>
+            </div>
+            <div className="HomeTemplate-content-left-mid">
+              <div className="content-card1">
+                <img src={logo}></img>
+                <p>user</p>
+                <h2>코딩테스트</h2>
+                <p>마감n일전</p>
+                <button>대회 신청하기</button>
+              </div>
+              <div className="content-card2">
+                <img src={logo}></img>
+                <p>user</p>
+                <h2>코딩테스트</h2>
+                <p>마감n일전</p>
+                <button>대회 신청하기</button>
+              </div>
+            </div>
           </div>
-          <div className="content-card-right">
-            <p>user</p>
-            <h2>코딩테스트</h2>
-            <p>마감n일전</p>
-            <button>대회 신청하기</button>
+          <div className="HomeTemplate-content-right">
+            <div className="HomeTemplate-content-right-top">
+              <div className="profilebase"></div>
+            </div>
+            <div className="HomeTemplate-content-right-mid"></div>
           </div>
         </div>
       </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
 const Footer = () => {
-    const thisYear = () => {
-        const year = new Date().getFullYear();
-        return year
-    };
-    return(
-        <div className="main-footer">
-            <p className="copyright">
-            Copyright &copy; Con-Con <span>{thisYear()}</span>
-            </p>
-        </div>
-    )
-}
+  const thisYear = () => {
+    const year = new Date().getFullYear();
+    return year;
+  };
+  return (
+    <div className="PageTemplate-footer">
+      <div className="Footer"></div>
+    </div>
+  );
+};
 export default Footer;
